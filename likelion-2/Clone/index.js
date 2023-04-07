bb930d3e328ff97517bef57bbe0ca618edf9d636
ahyoung <72968968+kcd50522@users.noreply.github.com>
2023-04-07 15:48:37 +0900

    Update index.js

--- likelion-2/Clone/index.js
+++ likelion-2/Clone/index.js
@@ -35,3 +35,58 @@ const registButtonEvent = () => {
     rightBtn.addEventListener('click', () => moveImage('right'));
 };
 
+
+/** @param {{url: string, id: string, title: string}} data */
+const renderImage = (data) => {
+    const divEl = document.createElement('div');
+    const imgEl = document.createElement('img');
+    const pEl = document.createElement('p');
+
+    imgEl.src = data.url;
+    imgEl.alt = '슬라이드이미지';
+
+    pEl.textContent = data.title;
+
+    divEl.append(imgEl);
+    divEl.append(pEl);
+    container.append(divEl);
+};
+
+/** @param {number} currentIndex  */
+const renderCounter = (currentCount) => {
+    counter.textContent = `${currentCount + 1} / ${totalCoount}`;
+};
+
+/** Carousel을 움직인다.
+    @param {'left' | 'right'} type 
+*/
+const moveImage = (type) => {
+    if (type === 'left') moveLeft();
+    if (type === 'right') moveRight();
+};
+
+const moveLeft = () => {
+    if (currentCount > 0) {
+        container.scrollTo({ left: --currentCount * 600, behavior: 'smooth'});
+    }
+    else {
+        container.scrollTo ({ left: totalCount * 600, behavior: 'smooth' });
+        currentCount = totalCount - 1;
+    }
+    renderCounter(currentCount);
+}
+
+const moveRight = () => {
+    if (currentCount < totalCount - 1) {
+    container.scrollTo({
+        left: ++currentCount * 600,
+        behavior: 'smooth',
+    });
+    } else {
+    container.scrollTo({ left: 0, behavior: 'smooth' });
+    currentCount = 0;
+    }
+    renderCounter(currentCount);
+};
+
+
